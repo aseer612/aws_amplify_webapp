5c05c90bc4c14ba9fc3040b6c3484ad0b51d325e
aseer612 <152047855+aseer612@users.noreply.github.com>
2024-07-19 20:50:35 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_ep3b91xlb', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5h2gqg1vhs4m1usfuvb665te0q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_P8dn7h5Ni', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6ehr1gmocqdk7d5lr92e322o2p', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
