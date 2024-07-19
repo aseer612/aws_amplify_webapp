e5e253a04d19cbc5983ebf9c7d66c40e844715cb
aseer612 <152047855+aseer612@users.noreply.github.com>
2024-07-19 14:21:49 +0530

    Update config.js
    changed client id and user pool id

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_i2LuXLb2z', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4fm07m38q7vnq5065vro3n8bhp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
+        userPoolId: 'ap-south-1_UwS80JuiA', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '22kced10a22h952q8tve7gplt9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://fs6mwndny4.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
