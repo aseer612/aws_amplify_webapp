7824fabfde852333658f60f45f116aecb6205c29
aseer612 <152047855+aseer612@users.noreply.github.com>
2024-07-19 15:04:23 +0530

    Update config.js
    invoke url

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://fs6mwndny4.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://cok3c8zq6l.execute-api.ap-south-1.amazonaws.com/dev',
     }
 };
